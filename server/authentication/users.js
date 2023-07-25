d7499bd1265e6d573db23e4d63ba4c01d113f543
Mohamed Ahmed <ma07041705@gmail.com>
2023-07-25 12:39:24 +0300

    rather than using hash password i have used generate-password

--- server/authentication/users.js
+++ server/authentication/users.js
@@ -19,6 +19,7 @@ const loginUser = async (req, res) => {
     const SchoolUsers = Users(schoolDB);
 
     const user = await SchoolUsers.findOne({ where: { username: username } });
+    console.log(user);
     if (!user) {
       return res
         .status(400)
